e2a317b43d875a3e898cc09f36c19917b9fc1ce1
David <161796631+CodeofGuff@users.noreply.github.com>
2024-10-16 17:18:28 -0700

    Learning

---
+++ testing.ts
@@ -0,0 +1,17 @@
+let aged = true;
+let realAge = 0;
+
+if (aged) {
+  realAge = 4;
+}
+
+let dogAge = realAge * 7;
+
+console.log(`${dogAge} years`);
+
+
+let sentence = "Why Hello therE!"
+
+let newS = sentence.toLowerCase()
+
+console.log(newS)
